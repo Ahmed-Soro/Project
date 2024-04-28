a55ac7815aeab0ec3f3d73afdefc4da581729b30
Ahmed-Soro <168356606+Ahmed-Soro@users.noreply.github.com>
2024-04-28 18:31:41 +0300

    Add files via upload

---
+++ product.js/product.js
@@ -0,0 +1,23 @@
+var SignFull = document.getElementById("Full");
+var SignGmail2 = document.getElementById("2mail");
+var SignPass = document.getElementById("2pass");
+var signRPass = document.getElementById("Rpass");
+var LoginGmail = document.getElementById("mail");
+var Loginpass = document.getElementById("pass");
+var Mass = document.getElementsByClassName("massg");
+
+
+
+
+function Check(){
+ if(signPass!=signRPass)
+ {
+Mass.innerHTML="doesn't similar";
+console.log("are you fine")
+throw error;
+
+ }   
+ else{
+    
+ }
+}
